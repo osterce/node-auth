53317d582198acae7a870b3d8239ffa96f0fbc09
Oscar Alvaro Terceros Beltrán <osterce@outlook.com>
2025-11-14 13:13:30 -0400

    Consumir el repositorio directamente

--- src/presentation/auth/controller.ts
+++ src/presentation/auth/controller.ts
@@ -1,15 +1,21 @@
 import { Request, Response } from 'express';
-import { RegisterUserDto } from '../../domain';
+import { AuthRepository, RegisterUserDto } from '../../domain';
+
 
 export class AuthController {
 
   //Inyeccion de dependencias
-  constructor() {}
+  constructor(
+    private readonly authRepository: AuthRepository
+  ) {}
 
   registerUser = ( req: Request, res: Response) => {
     const [error, registerUserDto] = RegisterUserDto.create( req.body );
     if( error ) return res.status(400).json({ error });
-    res.json( registerUserDto );
+
+    this.authRepository.register( registerUserDto! )
+      .then( user => res.json({ user }) )
+      .catch( error => res.status(500).json({ error: error.message }) );
   }
 
   loginUser = ( req: Request, res: Response) => {
--- src/presentation/auth/routes.ts
+++ src/presentation/auth/routes.ts
@@ -1,12 +1,16 @@
 import { Router } from "express";
 import { AuthController } from "./controller";
+import { MongoDataSourcesImpl, MongoRepositoryImpl } from "../../infrastructure";
 
 export class AuthRoutes {
 
   static get routes(): Router {
 
     const router = Router();
-    const controller = new AuthController();
+
+    const mongoDatasource = new MongoDataSourcesImpl();
+    const authRepository = new MongoRepositoryImpl( mongoDatasource );
+    const controller = new AuthController( authRepository );
 
     router.use('/login', controller.loginUser );
     router.use('/register', controller.registerUser ) ;
