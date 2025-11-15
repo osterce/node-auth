e0d975073647e670a6a87580164fd424c3d7cf40
Oscar Alvaro Terceros Beltrán <osterce@outlook.com>
2025-11-15 02:38:09 -0400

    Manejo de errores HTTP

--- src/presentation/auth/controller.ts
+++ src/presentation/auth/controller.ts
@@ -1,5 +1,5 @@
 import { Request, Response } from 'express';
-import { AuthRepository, RegisterUserDto } from '../../domain';
+import { AuthRepository, CustomError, RegisterUserDto } from '../../domain';
 
 
 export class AuthController {
@@ -9,13 +9,20 @@ export class AuthController {
     private readonly authRepository: AuthRepository
   ) {}
 
+  private handleError=( error: unknown, res: Response )=>{
+    if( error instanceof CustomError ){
+      return res.status( error.statusCode ).json({ error: error.message });
+    }
+    return res.status(500).json({ error: 'Internal server error' });
+  }
+
   registerUser = ( req: Request, res: Response) => {
     const [error, registerUserDto] = RegisterUserDto.create( req.body );
     if( error ) return res.status(400).json({ error });
 
     this.authRepository.register( registerUserDto! )
       .then( user => res.json({ user }) )
-      .catch( error => res.status(500).json({ error: error.message }) );
+      .catch( error => this.handleError( error, res ) );
   }
 
   loginUser = ( req: Request, res: Response) => {
