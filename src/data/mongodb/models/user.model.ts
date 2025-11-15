190512a2cd2c646152d665e8c4e10799df52b06b
Oscar Alvaro Terceros Beltrán <osterce@outlook.com>
2025-11-15 00:39:39 -0400

    Crear esquema y modelo de mongoose

---
+++ src/data/mongodb/models/user.model.ts
@@ -0,0 +1,29 @@
+import mongoose from "mongoose";
+import { UserEntity } from "../../../domain";
+
+const userSchema = new mongoose.Schema<UserEntity>({
+
+  name: {
+    type: String,
+    required: [ true, 'Name is required' ]
+  },
+  email: {
+    type: String,
+    required: [ true, 'Email is required' ],
+    unique: true
+  },
+  password: {
+    type: String,
+    required: [ true, 'Password is required' ]
+  },
+  img: {
+    type: String,
+  },
+  role: {
+    type: [String],
+    default: ['USER_ROLE'],
+    enum: ['ADMIN_ROLE', 'USER_ROLE'],
+    required: [ true, 'Roles are required' ] }
+});
+
+export const UserModel = mongoose.model<UserEntity>('User', userSchema);
